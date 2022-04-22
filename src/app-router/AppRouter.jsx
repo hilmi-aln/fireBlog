85ae78b7aae050590630487c23fefdf4347872b8
leon <akademikhlm@gmail.com>
2022-04-23 02:35:39 +0300

    login-v1

--- src/app-router/AppRouter.jsx
+++ src/app-router/AppRouter.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
   return (
     <Router>
       <Navbar />
-      {/* <Routes><Route path="/" component={Dashboard} /></Routes> */}
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />        
+      </Routes>
     </Router>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,91 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import cw from "../assets/cw.jpeg"
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { useState } from "react";
+import cw from "../assets/cw.jpeg";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => {
+export default function Navbar() {
+  // const [auth, setAuth] = useState(true);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <img src={cw} alt="cw" width="50" height="50" />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign:"center" }}>
-            {"<CLARUSWAY/>"}
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <img
+              src={cw}
+              alt="logo"
+              style={{ width: "2rem" }}
+              onClick={() => navigate("/")}
+            />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            style={{ textAlign: "center" }}
+          >
+            {"<FreeBlog/>"}
           </Typography>
-          <Avatar src="/broken-image.jpg" />
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={() => navigate("/login")}>Login</MenuItem>
+              <MenuItem onClick={() => navigate("/register")}>
+                Register
+              </MenuItem>
+              {/* <MenuItem onClick={handleClose}>Profil</MenuItem>
+                <MenuItem onClick={handleClose}>New</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem> */}
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
   );
-};
-
-export default Navbar;
+}
