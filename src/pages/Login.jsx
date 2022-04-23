9e176d8f1f9a7eb0c454e1f39943a6a8491effb0
leon <akademikhlm@gmail.com>
2022-04-23 03:14:29 +0300

    login and register last version

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import blog from "../assets/blok.png";
+import "./LoginStyle.css";
 
 
 function Login() {
   return (
-    <section classNameName="vh-100" style={{ backgroundColor: "#508bfc" }}>
-      <div className="container py-5 h-100">
+    <section className="vh-50" >
+      <div className="container py-5 h-80">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
             <div
@@ -12,7 +13,7 @@ function Login() {
               style={{ borderRadius: "1rem" }}
             >
               <div className="card-body p-5 text-center">
-                <h3 className="mb-5">Sign in</h3>
+                <img className="mb-5" src={blog} alt="image"/>
 
                 <div className="form-outline mb-4">
                   <input
@@ -36,20 +37,7 @@ function Login() {
                   </label>
                 </div>
 
-                {/* <!-- Checkbox --> */}
-                <div className="form-check d-flex justify-content-start mb-4">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="form1Example3"
-                  />
-                  <label className="form-check-label" for="form1Example3">
-                    {" "}
-                    Remember password{" "}
-                  </label>
-                </div>
-
+                
                 <button
                   className="btn btn-primary btn-lg btn-block"
                   type="submit"
@@ -66,14 +54,7 @@ function Login() {
                 >
                   <i className="fab fa-google me-2"></i> Sign in with google
                 </button>
-                <button
-                  className="btn btn-lg btn-block btn-primary mb-2"
-                  style={{ backgroundColor: "#3b5998" }}
-                  type="submit"
-                >
-                  <i className="fab fa-facebook-f me-2"></i>Sign in with
-                  facebook
-                </button>
+                
               </div>
             </div>
           </div>
